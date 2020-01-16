5ff00e4817d6a09eeb29794dc050fa06df667733
Joe Testa <jtesta@positronsecurity.com>
2020-01-16 17:04:07 -0500

    Added check for OpenSSL v1.1.x.

--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
 require 'openssl'
 
+# OpenSSL v1.1.x is required.
+openssl_version = OpenSSL::OPENSSL_LIBRARY_VERSION.split(' ')[1]
+if openssl_version.start_with? '1.0.' then
+  puts "Error: OpenSSL v1.1.x must be installed (you have #{openssl_version})."
+  exit -1
+end
+
 raw = File.read ARGV[0]
 ca = OpenSSL::X509::Certificate.new(raw) # Read certificate
 ca_key = ca.public_key # Parse public key from CA
